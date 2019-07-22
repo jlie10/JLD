eeb4e393ac7baea90dd33f9653eedd74170b34b6
mml <mml@debiant450s>
2019-07-22 16:01:45 +0200

    Adding files from JLD

---
+++ Patriot.py
@@ -0,0 +1,177 @@
+#!/usr/bin/env python##############################################################################
+# Patriot       www.dessalles.fr/Evolife              Jean-Louis Dessalles #
+#                 Telecom Paris  2019                    www.dessalles.fr    #
+# together with Julien Panis-Lie                                             #
+##############################################################################
+
+
+""" Emergence of conspicuous patriotism under treason risk.
+"""
+
+
+
+import sys
+import random
+import time	# for sleep
+
+sys.path.append('../../..')
+import Evolife.Tools.Tools as ET
+# import Evolife.Other.SocialNetwork.SocialSimulation as SSim
+import Evolife.Ecology.Observer as EO
+import Evolife.Ecology.Individual as EI
+import Evolife.Ecology.Group as EG
+import Evolife.Ecology.Population as EP
+import Evolife.Ecology.Alliances as EA
+import Evolife.Scenarii.Default_Scenario as EDS
+import Evolife.QtGraphics.Evolife_Window as EW
+
+class Scenario(EDS.Default_Scenario):
+	""" Implement a scenario here, by instantiating Default_Scenario.
+		See documentation in Default_Scenario.py to see how.
+	"""
+	
+	def __init__(self):
+		EDS.Default_Scenario.__init__(self, Name='Patriot', CfgFile='___Patriot.evo') # loads 'Patriot.evo'
+		
+	def genemap(self):
+		return [('DemandForPatriotism', 8),		# signal level expected from acquaintances
+				('PatriotSignal', 8),			# signal level if patriot
+				('NonPatriotSignal', 8),		# signal level if non-patriot
+				]
+	
+	def phenemap(self):
+		""" Defines the set of non inheritable characteristics """
+		return ['Patriotism']	# determines whether one is patriot or not
+			
+	def display_(self):
+		return [('blue', 'PatriotSignal', 'signal level if patriot'), 
+				 ('green', 'DemandForPatriotism', 'signal level expected from acquaintances'),
+				 ('red', 'NonPatriotSignal', 'signal level if non-patriot'),
+				 ]
+
+	def prepare(self, indiv):
+		# print('.', end='', flush=True)
+		indiv.score(100, FlagSet=True)
+		
+	def interaction(self, indiv, partner):
+		" Interaction between individuals "
+		PerceivedSignal = ET.noise_mult(partner.signal(), self['Noise'])
+		PartnerPerceivedSignal = ET.noise_mult(indiv.signal(), self['Noise'])
+		if PerceivedSignal >= indiv.demand() and PartnerPerceivedSignal >= partner.demand():
+			indiv.acquainted(partner)
+		
+	def evaluation(self, Indiv):
+		" Score changes due to friends & traitors "
+		# signalling cost
+		Indiv.signalCost()
+		# friends get rewarded for having a friend or penalized for being acquainted with a traitor 
+		Solidarity = 0	# benefit from having friends
+		for Friend in Indiv.Friends():
+			if Friend.traitor():	
+				Friend.score(self['TraitorBenefit'])
+				Indiv.score(-self['TreasonImpact'])
+				Solidarity = 0
+				break	# one treason only
+			else:
+				# cumulative contribution of friends with saturation (varies from 0 to 100)
+				Solidarity = 100 - (100 - Solidarity) * (100 - self['FriendImpact']) / 100.0
+		Indiv.score(-self['LonelinessPenalty'] + Solidarity * self['Solidarity'] / 100.0)
+		# penalty for remaining alone
+		# if Indiv.nbFriends() == 0:
+			# Indiv.score(Indiv.score() / self['LonelinessPenalty'])
+	
+	def lives(self, members):
+		" converts scores into life points "
+		super().lives(members)
+		# correction to make negative scores die
+		for indiv in members:
+			if indiv.score() < 0:	indiv.LifePoints = -1
+		
+	def update_positions(self, members, groupLocation):
+		for indiv in members:	
+			indiv.location = (groupLocation + indiv.Phene_value('Patriotism'), 
+				# indiv.score(), 'blue', 6)
+				indiv.signal(), 'blue', 6)
+		 
+	def Field_grid(self):	
+		return [(0, 98, 'green', 1, 100, 98, 'green', 1), (100, 100, 1, 0)]
+		
+
+class Individual(EI.EvolifeIndividual):
+	"   class Individual: defines what an individual consists of "
+
+	def __init__(self, Scenario, ID=None, Newborn=False):
+		super().__init__(Scenario=Scenario, ID=ID, Newborn=Newborn, 
+							MaxFriends=Scenario['MaxFriends'])
+		self.Signal = -1
+		self.Traitor = None
+		
+	def patriot(self):
+		return self.Phene_value('Patriotism') > 100 - self.Scenario['PatriotRatio']
+		
+	def traitor(self):
+		if self.Traitor is None:
+			self.Traitor = False
+			if not self.patriot():	
+				# Non-patriot have a probability of turning into traitors
+				self.Traitor = random.randint(0,100) < self.Scenario['TreasonProbability']
+		return self.Traitor
+	
+	def signal(self):
+		" computes signal level "
+		if self.Signal < 0:
+			if self.patriot():	self.Signal = self.gene_relative_value('PatriotSignal')
+			else:	
+				self.Signal = min(self.gene_relative_value('NonPatriotSignal'),
+					self.Scenario['TraitorMaxSIgnal'])
+		return self.Signal
+
+	def signalCost(self):
+		" paying signalling cost "
+		self.score(-self.Scenario['SignallingCost'] * self.signal() / 100.0)		# signalling cost
+	
+	def demand(self):
+		return self.gene_relative_value('DemandForPatriotism')
+	
+class Group(EG.EvolifeGroup):
+	" Calls local class when creating individuals "
+		
+	def createIndividual(self, ID=None, Newborn=True):
+		# calling local class
+		return Individual(self.Scenario, ID=self.free_ID(Prefix=''), Newborn=Newborn)
+
+		
+class Population(EP.EvolifePopulation):
+	" Calls local class when creating group "
+
+	def createGroup(self, ID=0, Size=0):
+		return Group(self.Scenario, ID=ID, Size=Size)	# local class
+
+	def season_initialization(self):
+		for gr in self.groups:
+			for agent in gr:
+				# agent.lessening_friendship()	# eroding past gurus performances
+				if self.Scenario['EraseNetwork']:	agent.forgetAll()
+				agent.reinit()
+		
+	
+if __name__ == "__main__":
+	Gbl = Scenario()
+	Obs = EO.EvolifeObserver(Gbl)
+	Pop = Population(Scenario=Gbl, Evolife_Obs=Obs)
+	if Gbl['BatchMode'] == 0:	print(__doc__)
+	
+	# launching windows
+	# See Evolife_Window.py for details
+	Capabilities='FGCNP'	# F = 'Field'; G = 'Genomes'; C = 'Curves'; 
+	Views = []
+	if 'F' in Capabilities:	Views.append(('Field', 500, 350))	# start with 'Field' window on screen
+	if 'T' in Capabilities:	Views.append(('Trajectories', 500, 350))	# 'Trajectories' on screen
+	if 'N' in Capabilities:	Views.append(('Network', 500, 150))	# 'Network' on screen
+	Obs.recordInfo('DefaultViews',	Views)	# Evolife should start with these window open
+	EW.Start(SimulationStep=Pop.one_year, Obs=Obs, Capabilities=Capabilities, 
+		Options=[('Background','green11')])
+	time.sleep(1.1)
+
+
+__author__ = 'Dessalles'
---
+++ Sacrifice.py
@@ -0,0 +1,155 @@
+#!/usr/bin/env python##############################################################################
+# Sacrifice       www.dessalles.fr/Evolife              Jean-Louis Dessalles #
+#                 Telecom Paris  2019                    www.dessalles.fr    #
+# together with Julien Panis-Lie                                             #
+##############################################################################
+
+
+""" Could self-sacrifice therefore have a biological function? This project sets the groundwork for investigating the (individual) biological motivations that may underlie self-sacrifice (which need not coincide with its moral motivations, potentially geared towards the collective).
+"""
+
+
+
+import sys
+import random
+import time	# for sleep
+
+sys.path.append('../../..')
+import Evolife.Tools.Tools as ET
+# import Evolife.Other.SocialNetwork.SocialSimulation as SSim
+import Evolife.Ecology.Observer as EO
+import Evolife.Ecology.Individual as EI
+import Evolife.Ecology.Group as EG
+import Evolife.Ecology.Population as EP
+import Evolife.Ecology.Alliances as EA
+import Evolife.Scenarii.Default_Scenario as EDS
+import Evolife.QtGraphics.Evolife_Window as EW
+
+class Scenario(EDS.Default_Scenario):
+	""" Implement a scenario here, by instantiating Default_Scenario.
+		See documentation in Default_Scenario.py to see how.
+	"""
+	
+	def __init__(self):
+		EDS.Default_Scenario.__init__(self, Name='Sacrifice', CfgFile='___Sacrifice.evo') # loads 'Sacrifice.evo'
+		
+	def genemap(self):
+		return [('Readiness', 8)]	# Readiness to display patriotism
+	
+	def phenemap(self):
+		""" Defines the set of non inheritable characteristics """
+		return ['Patriotism', 'Risk']
+			
+	def display_(self):
+		return [('blue', 'Readiness', 'Readiness to display patriotism'), 
+				 ('green', 'Risk', 'Risk taking'),
+				 ('red', 'NbSuicides', '# suicides per year'),
+				 ]
+
+	def initialization(self):
+		self.NbSuicides = 0	# keeps track of suicides
+		
+	def start_game(self, members):
+		for indiv in members:
+			for child in indiv.Children.copy():
+				if not child in members:	# child is dead
+					indiv.Children.remove(child)
+					continue
+				for sibling in indiv.Children:
+					if not sibling in members:	# sibling is dead
+						continue
+					if sibling != child:	
+						child.Siblings.add(sibling)	
+		self.NbSuicides = 0	# keeps track of suicides
+		super().start_game(members)
+		
+	def prepare(self, indiv):
+		indiv.update_()
+
+	def evaluation(self, indiv):
+		# if Obs.Visible():	print('%d-%d' % (len(indiv.Children), len(indiv.Siblings)), end=' ', flush=True) 
+		if indiv.suicide():	# most resolute individual may commit suicide
+			self.NbSuicides += 1
+
+	def end_game(self, members):
+		self.NbSuicides /= self['PopulationSize']
+		
+	def update_positions(self, members, groupLocation):
+		for indiv in members:	
+			indiv.location = (groupLocation + indiv.Phene_value('Patriotism'), 
+				# indiv.Phene_value('Risk'), 'blue', 6)
+				indiv.score(), 'blue', 6)
+
+	# # # def parenthood(self, RankedCandidates, Def_Nb_Children):
+		# # # Candidates = super().parenthood(RankedCandidates, Def_Nb_Children)
+		# # # # filtering out individuals with score==0 ("dead" by suicide)
+		# # # return [C for C in Candidates if C[0].score() > 0]
+
+	def new_agent(self, child, parents):
+		" initializes newborns - parents==None when the population is created"
+		for P in parents: P.Children.add(child)	# let know parents who is their child
+		return True
+		 
+	def Field_grid(self):	
+		return [(0, 98, 'green', 1, 100, 98, 'green', 1), (100, 100, 1, 0)]
+		
+		
+class Individual(EI.EvolifeIndividual):
+	"   class Individual: defines what an individual consists of "
+
+	def __init__(self, Scenario=None, ID=None, Newborn=False, maxQuality=100):
+		super().__init__(Scenario=Scenario, ID=ID, Newborn=Newborn)
+		# if not Newborn:	self.Phene_value('Patriotism', 100.0 * int(self.ID) / maxQuality) 
+		self.Children = set()
+		self.Siblings = set()
+		self.Phene_value('Risk', 0)
+		self.score(100, FlagSet=True)
+		
+	def update_(self):
+		self.Phene_value('Risk', self.Phene_value('Patriotism') * self.gene_relative_value('Readiness') / 500.0)
+		# if Obs.Visible():	
+			# print((self.Phene_value('Patriotism'), self.gene_relative_value('Readiness') ), flush=True)
+
+	def suicide(self):
+		if self.score() > 0 and random.randint(0, 100) < self.Phene_value('Risk'):
+			self.score(0, FlagSet=True)
+			# let children benefit from suicide
+			for child in self.Children:	child.score(self.Scenario['ChildrenBonus'])
+			for sibling in self.Siblings:	sibling.score(self.Scenario['SiblingBonus'])
+			return True
+		return False
+	
+class Group(EG.EvolifeGroup):
+	" Calls local class when creating individuals "
+		
+	def createIndividual(self, ID=None, Newborn=True):
+		# calling local class
+		return Individual(self.Scenario, ID=self.free_ID(Prefix=''), Newborn=Newborn)
+
+		
+class Population(EP.EvolifePopulation):
+	" Calls local class when creating group "
+
+	def createGroup(self, ID=0, Size=0):
+		return Group(self.Scenario, ID=ID, Size=Size)	# local class
+
+	
+if __name__ == "__main__":
+	Gbl = Scenario()
+	Obs = EO.EvolifeObserver(Gbl)
+	Pop = Population(Scenario=Gbl, Evolife_Obs=Obs)
+	if Gbl['BatchMode'] == 0:	print(__doc__)
+	
+	# launching windows
+	# See Evolife_Window.py for details
+	Capabilities='FGCP'	# F = 'Field'; G = 'Genomes'; C = 'Curves'; 
+	Views = []
+	if 'F' in Capabilities:	Views.append(('Field', 500, 350))	# start with 'Field' window on screen
+	if 'T' in Capabilities:	Views.append(('Trajectories', 500, 350))	# 'Trajectories' on screen
+	Obs.recordInfo('DefaultViews',	Views)	# Evolife should start with these window open
+	EW.Start(SimulationStep=Pop.one_year, Obs=Obs, Capabilities=Capabilities, 
+		Options=[('Background','green11')])
+	time.sleep(1.1)
+
+
+__author__ = 'Dessalles'
